299e01deee07c789c91d435ab5d960c26ef3769d
yacob <yacob.huang@gmail.com>
2022-06-23 10:18:40 +0700

    contact to email

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,77 +1,82 @@
 <template lang="pug">
 .contact
-    PageHeader(:image="data.headingImage" :heading1="data.heading1" :heading2="data.heading2")
+    PageHeader(:image="strapiImage($axios.defaults.baseURL, page.headerBackground)" :heading1="page.header1" :heading2="page.header2" v-if="page.headerBackground!=null")
     b-container.section
         b-card.contact__office.mb-4
             b-row
                 b-col(cols="12" sm="6")
-                    b-img.office__map(:src="data.map")
-                b-col(cols="12" sm="6")
+                    b-img.office__map(:src="strapiImage($axios.defaults.baseURL, page.mapImage)" v-if="page.mapImage!=null")
+                b-col(cols="12" sm="6" v-if="page.headOffice")
                     .office__title Head Office
-                    .office__name.mb-2 PT MCS Internasional
-                    .office__item {{data.addressLine1}}
-                    .office__item {{data.addressLine2}}
-                    .office__item {{data.addressLine3}}
-                    .office__item Phone: {{data.phone}}
-                    .office__item Fax: {{data.fax}}
-                    .office__item Email: {{data.email}}
+                    .office__name.mb-2 {{page.headOffice.label}}
+                    .office__item {{page.headOffice.addressLine1}}
+                    .office__item {{page.headOffice.addressLine2}}
+                    .office__item {{page.headOffice.addressLine3}}
+                    .office__item Phone: {{page.headOffice.phone}}
+                    .office__item Fax: {{page.headOffice.fax}}
+                    .office__item Email: {{page.headOffice.email}}
         b-card.contact__email
             .email__heading Reach Us on Email
             b-form.email__form
                 b-row
                     b-col(cols="12" sm="6")
                         b-form-group(label="Full Name")
-                            b-form-input(type="text")
+                            b-form-input(type="text" v-model="message.name")
                         b-form-group(label="Phone")
-                            b-form-input(type="number")
+                            b-form-input(type="number"  v-model="message.phone")
                         b-form-group(label="Email")
-                            b-form-input(type="email")
+                            b-form-input(type="email" v-model="message.email")
                     b-col(cols="12" sm="6")
                         b-form-group(label="Subject")
-                            b-form-input(type="text")
+                            b-form-input(type="text"  v-model="message.subject")
                         b-form-group(label="Message")
-                            b-form-textarea(type="text" rows="3")
-                b-button.float-right(variant="primary") Submit
+                            b-form-textarea(type="text" rows="3" v-model="message.message")
+                b-button.float-right(
+                    variant="primary"
+                    @click="sendEmail(message.subject, message.message, message.name, message.phone, message.email)") Submit
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import CardVacancy from '@/components/CardVacancy.vue'
 import PageHeader from '@/components/PageHeader.vue'
-import Thumbnail from '@/components/Thumbnail.vue'
-const mock = {
-    headingImage: require('@/assets/img/videotron-image.jpg'),
-    heading1: 'Keep In Touch',
-    heading2: 'Contact Us',
-    sectionTitle: 'Available Vacancies',
-    sectionBody: 'Submit your CV and grow your career with us',
-    addressLine1: 'Granadi Building 5th floor',
-    addressLine2: 'Jl. HR Rasuna Said Blok X-1',
-    addressLine3: 'Kav 8-9, Jakarta 12950, Indonesia',
-    phone: '+62212525196',
-    fax: '+622152964555',
-    email: 'crewing@mcs-international.com',
-    map: require('@/assets/img/map.png')
-}
+import { strapiImage } from '@/utilities/StrapiImage'
 export default Vue.extend({
     name: 'contact',
     layout: 'SinglePage',
     components: {
-        CardVacancy,
         PageHeader,
-        Thumbnail
     },
     data: () => {
         return {
-            data: mock,
-            dataSelected: {
-                image: '',
-                year: '',
-                label: '',
-                description: ''
-            }
+            message: {
+                name: '',
+                phone: '',
+                email: '',
+                subject: '',
+                message: ''
+            },
+            page: {},
         }
     },
+    methods: {
+        async getPage() {
+            try {
+                let page = await this.$axios.$get('/api/page-contact-us?populate=*')
+                this.page = page.data.attributes
+            } catch (error) { } 
+        },
+        sendEmail(subject: string, message: string, name: string, phone: string, email: string) {
+            let mailLink = 'mailto:yacob.huang@gmail.com' 
+                            + '?subject=' + encodeURIComponent(subject)
+                            + '&body=' +  encodeURIComponent(name) + ' - (' + encodeURIComponent(phone) + ', ' + encodeURIComponent(email) + ')%0D%0D'
+                            + encodeURIComponent(message);
+            window.location.href = mailLink
+        },
+        strapiImage
+    },
+    mounted() {
+        this.getPage()
+    }
 })
 </script>
 <style lang="scss" scoped>
