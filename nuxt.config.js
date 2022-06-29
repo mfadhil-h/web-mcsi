eccdf0f42760948b7bba13c965b4a7cc966159ee
yacob <yacob.huang@gmail.com>
2022-06-29 21:13:10 +0700

    modify header dropdown

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap' }
     ]
   },
