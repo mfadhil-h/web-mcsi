a0a6e3b694d7fd63a0df0cc9f249daeef06ae853
Zhaky Hadi <zhaky.gng@gmail.com>
2022-07-03 23:47:00 +0800

    reduce CPU Usage

--- utilities/ParticleOptions.js
+++ utilities/ParticleOptions.js
@@ -4,36 +4,36 @@ export default {
             value: '#FAFAFA'
         }
     },
-    fpsLimit: 60,
-    interactivity: {
-        detectsOn: 'canvas',
-        events: {
-            onClick: {
-                enable: true,
-                mode: 'push'
-            },
-            onHover: {
-                enable: true,
-                mode: 'repulse'
-            },
-            resize: true
-        },
-        modes: {
-            bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40
-            },
-            push: {
-                quantity: 4
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4
-            }
-        }
-    },
+    fpsLimit: 30,
+    // interactivity: {
+    //     detectsOn: 'canvas',
+    //     events: {
+    //         onClick: {
+    //             enable: true,
+    //             mode: 'push'
+    //         },
+    //         onHover: {
+    //             enable: true,
+    //             mode: 'repulse'
+    //         },
+    //         resize: true
+    //     },
+    //     modes: {
+    //         bubble: {
+    //             distance: 400,
+    //             duration: 2,
+    //             opacity: 0.8,
+    //             size: 40
+    //         },
+    //         push: {
+    //             quantity: 4
+    //         },
+    //         repulse: {
+    //             distance: 200,
+    //             duration: 0.4
+    //         }
+    //     }
+    // },
     particles: {
         color: {
             value: '#666666'
@@ -61,7 +61,7 @@ export default {
                 enable: true,
                 value_area: 800
             },
-            value: 80
+            value: 30
         },
         opacity: {
             value: 0.5
