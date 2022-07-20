07cb0bde670f92d7381ee45c94f19c55261bc6b4
yacob <yacob.huang@gmail.com>
2022-07-20 11:08:54 +0700

    update head-title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'MCS International',
+    title: 'MCS Internasional',
     htmlAttrs: {
       lang: 'en'
     },
