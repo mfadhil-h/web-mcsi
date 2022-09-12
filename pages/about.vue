ac7fe833b8f8b99154f2c853df50f5b297960798
yacob <yacob.huang@gmail.com>
2022-09-12 16:31:34 +0700

    t about

--- pages/about.vue
+++ pages/about.vue
@@ -1,87 +1,97 @@
 <template lang="pug">
 .company-profile
-  PageHeader(
-    :image="strapiImage($axios.defaults.baseURL, page.headerBackground)",
-    :heading1="page.header1",
-    :heading2="page.header2",
-    v-if="page.headerBackground != null"
-  )
-  b-container(fluid)
-    b-container.section(v-if="page")
-      b-row
-        b-col.text-center.mb-4(cols="12")
-          .section__title {{ page.sectionTitle }}
-      b-row.mt-4
-        b-col(cols="12", md="5")
-          .section__body.mt-0.mb-4 {{ page.sectionDescription }}
-        b-col(cols="12", md="7")
-          b-row
-            b-col(cols="12")
-              youtube-player(:src="page.youtubeLink")
-            b-col(cols="12", md="6")
-              b-img.company__image(
-                :src="strapiImage($axios.defaults.baseURL, page.image1)",
-                v-if="page.image1"
-              )
-            b-col(cols="12", md="6")
-              b-img.company__image(
-                :src="strapiImage($axios.defaults.baseURL, page.image2)",
-                v-if="page.image2"
-              )
-  b-container.company__vision(fluid)
-    .ribbon
-    b-container.section(v-if="page")
-      b-card-group(deck)
-        b-card.vision(no-body)
-          b-card-body
-            .vision__icon
-              font-awesome-icon.icon__fa(icon="eye")
-            .vision__label Visi
-            .vision__description {{ page.visionShort }}
-          b-card-footer
-            .vision__link(v-b-modal.modal-vision)
-              span Selengkapnya
-              font-awesome-icon.ml-2(icon="circle-arrow-right")
-        b-card.vision(no-body)
-          b-card-body
-            .vision__icon
-              font-awesome-icon.icon__fa(icon="bullseye")
-            .vision__label Misi
-            .vision__description {{ page.missionShort }}
-          b-card-footer
-            .vision__link(v-b-modal.modal-mission)
-              span Selengkapnya
-              font-awesome-icon.ml-2(icon="circle-arrow-right")
-        b-card.vision(no-body)
-          b-card-body
-            .vision__icon
-              font-awesome-icon.icon__fa(icon="trophy")
-            .vision__label Nilai Perusahaan
-            .vision__description {{ page.companyValueShort }}
-          b-card-footer
-            b-link.vision__link(:to="'/company-value'")
-              span Selengkapnya
-              font-awesome-icon.ml-2(icon="circle-arrow-right")
-  b-modal#modal-vision(size="lg", hide-footer, centered, title="Visi")
-    .modal-vision__content(
-      v-html="micromark(page.visionLong)",
-      v-if="page.visionLong"
-    )
-  b-modal#modal-mission(size="lg", hide-footer, centered, title="Misi")
-    .modal-vision__content(
-      v-html="micromark(page.missionLong)",
-      v-if="page.missionLong"
-    )
-  b-modal#modal-company-value(
-    size="lg",
-    hide-footer,
-    centered,
-    title="Nilai Perusahaan"
-  )
-    .modal-vision__content(
-      v-html="micromark(page.companyValueLong)",
-      v-if="page.companyValueLong"
-    )
+   PageHeader(
+      :image='strapiImage($axios.defaults.baseURL, page.headerBackground)',
+      :heading1='page.header1',
+      :heading2='page.header2',
+      v-if='page.headerBackground'
+   )
+   b-container(fluid)
+      b-container.section(v-if='page')
+         b-row
+            b-col.text-center.mb-4(cols='12')
+               .section__title {{ page.sectionTitle }}
+         b-row.mt-4
+            b-col(cols='12', md='5')
+               .section__body.mt-0.mb-4 {{ page.sectionDescription }}
+            b-col(cols='12', md='7')
+               b-row
+                  b-col(cols='12')
+                     youtube-player(:src='page.youtubeLink')
+                  b-col(cols='12', md='6')
+                     b-img.company__image(
+                        :src='strapiImage($axios.defaults.baseURL, page.image1)',
+                        v-if='page.image1'
+                     )
+                  b-col(cols='12', md='6')
+                     b-img.company__image(
+                        :src='strapiImage($axios.defaults.baseURL, page.image2)',
+                        v-if='page.image2'
+                     )
+   b-container.company__vision(fluid)
+      .ribbon
+      b-container.section(v-if='page')
+         b-card-group(deck)
+            b-card.vision(no-body)
+               b-card-body
+                  .vision__icon
+                     font-awesome-icon.icon__fa(icon='eye')
+                  .vision__label {{ $t("vision") }}
+                  .vision__description {{ page.visionShort }}
+               b-card-footer
+                  .vision__link(v-b-modal.modal-vision)
+                     span {{ $t("more") }}
+                     font-awesome-icon.ml-2(icon='circle-arrow-right')
+            b-card.vision(no-body)
+               b-card-body
+                  .vision__icon
+                     font-awesome-icon.icon__fa(icon='bullseye')
+                  .vision__label {{ $t("mission") }}
+                  .vision__description {{ page.missionShort }}
+               b-card-footer
+                  .vision__link(v-b-modal.modal-mission)
+                     span {{ $t("more") }}
+                     font-awesome-icon.ml-2(icon='circle-arrow-right')
+            b-card.vision(no-body)
+               b-card-body
+                  .vision__icon
+                     font-awesome-icon.icon__fa(icon='trophy')
+                  .vision__label {{ $t("value") }}
+                  .vision__description {{ page.companyValueShort }}
+               b-card-footer
+                  b-link.vision__link(:to='"/company-value"')
+                     span {{ $t("more") }}
+                     font-awesome-icon.ml-2(icon='circle-arrow-right')
+   b-modal#modal-vision(
+      size='lg',
+      hide-footer,
+      centered,
+      :title='$t("vision")'
+   )
+      .modal-vision__content(
+         v-html='micromark(page.visionLong)',
+         v-if='page.visionLong'
+      )
+   b-modal#modal-mission(
+      size='lg',
+      hide-footer,
+      centered,
+      :title='$t("mission")'
+   )
+      .modal-vision__content(
+         v-html='micromark(page.missionLong)',
+         v-if='page.missionLong'
+      )
+   b-modal#modal-company-value(
+      size='lg',
+      hide-footer,
+      centered,
+      :title='$t("value")'
+   )
+      .modal-vision__content(
+         v-html='micromark(page.companyValueLong)',
+         v-if='page.companyValueLong'
+      )
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -183,3 +193,19 @@ export default Vue.extend({
    text-align: justify;
 }
 </style>
+<i18n>
+{
+   "id": {
+      "vision": "Visi",
+      "mission": "Misi",
+      "value": "Nilai Perusahaan",
+      "more": "Selengkapnya"
+   },
+   "en": {
+      "vision": "Vision",
+      "mission": "Mission",
+      "value": "Corporate Value",
+      "more": "Read More"
+   }
+}
+</i18n>
