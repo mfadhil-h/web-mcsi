be9fcae7b28e078faca032291c46181641f269bf
yacob <yacob.huang@gmail.com>
2022-09-12 16:59:19 +0700

    t milestone

--- pages/milestone.vue
+++ pages/milestone.vue
@@ -1,45 +1,47 @@
 <template lang="pug">
 .milestone
-  PageHeader(
-    :image="strapiImage($axios.defaults.baseURL, page.headerBackground)",
-    :heading1="page.header1",
-    :heading2="page.header2",
-    v-if="page.headerBackground != null"
-  )
-  b-container(fluid)
-    b-container.section.section--reading.text-center
-      b-card.section__bg(no-body)
-        .section__title {{ page.sectionTitle }}
-        .section__body {{ page.sectionDescription }}
-  b-container
-    .milestone
-      timeline-responsive.timeline(
-        v-for="(item, index) of milestones",
-        :key="index",
-        :title="item.attributes.title",
-        icon="calendar",
-        :timestamp="dayjs(item.attributes.date).format('YYYY')",
-        :inverted="index % 2 == 0",
-        @click="showModalMilestone(strapiImage($axios.defaults.baseURL, item.attributes.image), item.attributes.date, item.attributes.title, item.attributes.description)"
-      )
-        b-img.timeline__image(
-          :src="strapiImage($axios.defaults.baseURL, item.attributes.image)"
-        )
-        .timeline__details
-          .timeline__title {{ item.attributes.title }}
-          small.text-muted.timeline__timestamp {{ dayjs(item.attributes.date).format('YYYY') }}
-          .timeline__description
-            p.mb-0 {{ item.attributes.description.substring(0, 36) + ' ...' }}
-            .timeline__link
-              span Selengkapnya
-              font-awesome-icon.fw.title__arrow.ml-2(icon="circle-arrow-right")
-  b-modal#modal-milestone(size="lg", hide-footer, centered)
-    b-container.milestone-detail(fluid)
-      .text-center
-        b-img.milestone-detail__image(:src="milestoneSelected.image")
-        .milestone-detail__title {{ milestoneSelected.title }}
-        .milestone-detail__date {{ dayjs(milestoneSelected.date).format('YYYY') }}
-      .milestone-detail__description {{ milestoneSelected.description }}
+   PageHeader(
+      :image='strapiImage($axios.defaults.baseURL, page.headerBackground)',
+      :heading1='page.header1',
+      :heading2='page.header2',
+      v-if='page.headerBackground'
+   )
+   b-container(fluid)
+      b-container.section.section--reading.text-center
+         b-card.section__bg(no-body)
+            .section__title {{ page.sectionTitle }}
+            .section__body {{ page.sectionDescription }}
+   b-container
+      .milestone
+         timeline-responsive.timeline(
+            v-for='(item, index) of milestones',
+            :key='index',
+            :title='item.attributes.title',
+            icon='calendar',
+            :timestamp='dayjs(item.attributes.date).format("YYYY")',
+            :inverted='index % 2 == 0',
+            @click='showModalMilestone(strapiImage($axios.defaults.baseURL, item.attributes.image), item.attributes.date, item.attributes.title, item.attributes.description)'
+         )
+            b-img.timeline__image(
+               :src='strapiImage($axios.defaults.baseURL, item.attributes.image)'
+            )
+            .timeline__details
+               .timeline__title {{ item.attributes.title }}
+               small.text-muted.timeline__timestamp {{ dayjs(item.attributes.date).format("YYYY") }}
+               .timeline__description
+                  p.mb-0 {{ item.attributes.description.substring(0, 36) + " ..." }}
+                  .timeline__link
+                     span Selengkapnya
+                     font-awesome-icon.fw.title__arrow.ml-2(
+                        icon='circle-arrow-right'
+                     )
+   b-modal#modal-milestone(size='lg', hide-footer, centered)
+      b-container.milestone-detail(fluid)
+         .text-center
+            b-img.milestone-detail__image(:src='milestoneSelected.image')
+            .milestone-detail__title {{ milestoneSelected.title }}
+            .milestone-detail__date {{ dayjs(milestoneSelected.date).format("YYYY") }}
+         .milestone-detail__description {{ milestoneSelected.description }}
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -47,7 +49,7 @@ import * as dayjs from 'dayjs'
 import PageHeader from '@/components/PageHeader.vue'
 import { strapiImage } from '@/utilities/StrapiImage'
 export default Vue.extend({
-   name: 'Milestone',
+   name: 'PageMilestone',
    components: {
       PageHeader
    },
