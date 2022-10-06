85d07112d08776369b77cd2582303f6ae83d26ef
yacob <yacob.huang@gmail.com>
2022-10-06 10:29:36 +0700

    gtag context router

--- plugins/ga.js
+++ plugins/ga.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 export default (context, inject) => {
-   Vue.use(VueGtag, {
-      config: { id: 'G-0F2X9FF5ZB' }
-   })
+   Vue.use(
+      VueGtag,
+      {
+         config: {
+            id: process.env.NODE_ENV === 'dev' ? 'G-V2CVQ5L1W4' : 'G-0F2X9FF5ZB'
+         }
+      },
+      context.app.router
+   )
 
    context.$gtag = Vue.$gtag
    inject('gtag', Vue.$gtag)
