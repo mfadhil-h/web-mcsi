ff39e9a38798508b93ed504395f3120639b0ecd8
yacob <yacob.huang@gmail.com>
2022-12-02 14:53:38 +0700

    Change prod gtag id

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
+const gtagDev = 'G-V2CVQ5L1W4'
+// const gtagProdLegacy = 'G-0F2X9FF5ZB'
+const gtagProd = 'GTM-N88CJQP'
+
 export default (context, inject) => {
    Vue.use(
       VueGtag,
       {
          config: {
-            id:
-               process.env.nodeEnv === 'development'
-                  ? 'G-V2CVQ5L1W4'
-                  : 'G-0F2X9FF5ZB'
+            id: process.env.nodeEnv === 'development' ? gtagDev : gtagProd
          }
       },
       context.app.router
